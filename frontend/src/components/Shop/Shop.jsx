472148d68290c06c6adf766d1b5a3307d020daac
idubrovin <ivandubrovin2@mail.ru>
2020-03-11 21:59:18 +0300

    add shop-page grid

--- frontend/src/components/Shop/Shop.jsx
+++ frontend/src/components/Shop/Shop.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+    categoriesMenuWrapper: {
+        width: '85%',
+        minHeight: 'calc(100vh - 53x)c',
+        background: '#ccc',
+        float: 'right'
+    },
+    productsListWrapper: {
+        width: '15%',
+        minHeight: 'calc(100vh - 53px)',
+        background: '#c00',
+        marginRight: '70%'
+    }
+}));
+
 export const Shop = () => {
+    const classes = useStyles();
+
     return (
         <React.Fragment>
-            <p>Магазин</p>
+            <div className={classes.categoriesMenuWrapper}></div>
+            <div className={classes.productsListWrapper}></div>
         </React.Fragment>
     )
 };
