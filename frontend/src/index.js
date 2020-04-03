ddcabee3c2eeba759e7ed31925092231d3392e1d
idubrovin <ivandubrovin2@mail.ru>
2020-04-03 14:56:48 +0300

    Fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import App from "./App";
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import App from "./App"
 import "./index.css"
-import { store } from "./redux/store";
-import * as serviceWorker from "./serviceWorker";
+import { store } from "./redux/store"
+import * as serviceWorker from "./serviceWorker"
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-
     </Provider>,
     document.getElementById('root')
-);
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
